56bb211eb96ffe03f35cd85f96cb1a94ae2fed78
Harun23H <147631933+Harun23H@users.noreply.github.com>
2024-02-01 15:49:42 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,49 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 5000); // Change image every 5 seconds
+// }
+
+
+const slider = document.getElementById("myPrice");
+let displayPrice = document.getElementById("price");
+displayPrice.innerHTML = slider.value;
+ slider.oninput = function() {
+ displayPrice.innerHTML = this.value
+}
+
+
+
+
+
+
+// var slider = document.getElementById("myPrice");
+// var displayPrice = document.getElementById("price");
+// displayPrice.innerHTML = slider.value;
+// slider.oninput = function() {
+//   displayPrice.innerHTML = this.value;
+// }
